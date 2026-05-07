1238e909f52818c7051e8a7dfc7bad43f64090e1
Renaud Bourassa <to.rhino@gmail.com>
2026-05-07 13:19:27 -0400

    Update package

--- bixistrava/bixi/__init__.py
+++
@@ -1,5 +0,0 @@
-from .bixi import Bixi
-from .station import Station
-from .trip import Trip
-
-__all__ = ['Bixi', 'Station', 'Trip']
--- bixistrava/strava/__init__.py
+++
@@ -1,3 +0,0 @@
-from .strava import Strava
-
-__all__ = ['Strava']
