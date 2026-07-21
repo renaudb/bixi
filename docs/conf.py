8f31ce1b6647f3264098087c37de882d18d26916
Renaud Bourassa <rbourassadenis@gmail.com>
2026-07-21 14:11:42 -0400

    Add Read the Docs / Sphinx configuration
    Configures Sphinx with autodoc and napoleon to build API docs from the
    Google-style docstrings in bixi/bixi.py, and wires up Read the Docs to
    build them.

---
+++ docs/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "bixi"
+copyright = "2026, Renaud Bourassa"
+author = "Renaud Bourassa"
+
+# -- General configuration ----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+# -- Napoleon settings ---------------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/napoleon.html
+
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+
+# -- Options for HTML output ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
---
+++ docs/index.rst
@@ -0,0 +1,13 @@
+Bixi API Client
+===============
+
+Simple Bixi API Client.
+
+.. automodule:: bixi.bixi
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
